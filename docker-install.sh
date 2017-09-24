b8c9f042654ec4f118ebbfcf77008d69b104cf2d
Adam Mertz <adammertz@gmail.com>
2017-09-23 22:48:35 -0500

    add repo command

--- docker-install.sh
+++ docker-install.sh
@@ -20,5 +20,10 @@ else
     echo "Invalid fingerprint"
 fi
 
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
 apt-get update && apt-get install -y docker-ce
 docker run hello-world # TODO: autocheck this work
