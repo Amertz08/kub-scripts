da7101b7407c8aa6b6fd757c6206ad22b3a412fe
Adam Mertz <adammertz@gmail.com>
2017-09-23 23:06:16 -0500

    removed functions from script

--- kube-install.sh
+++ kube-install.sh
@@ -1,37 +1,26 @@
 #!/usr/bin/env bash
 
-install() {
-    echo "Installing Kubernetes"
-    curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-    chmod +x ./kubectl
-    mv ./kubectl /usr/local/bin/kubectl
-    echo "source <(kubectl completion bash)" >> ~/.bashrc
-    apt-get update && apt-get install -y apt-transport-https
-    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-    cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-    deb http://apt.kubernetes.io/ kubernetes-xenial main
-    EOF
-    apt-get update
-    apt-get install -y kubelet kubeadm
-}
+echo "Installing Kubernetes"
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+echo "source <(kubectl completion bash)" >> ~/.bashrc
 
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
 
-master() {
+apt-get update
+apt-get install -y kubelet kubeadm
+
+echo ""
+echo "Is this a master node [y/n]?"
+read master
+
+if [ master == "y" ]; then
     echo "Initializing master node"
     kubeadm init --pod-network-cidr
-}
+fi
 
-
-main() {
-    install
-
-    echo ""
-    echo "Is this a master node [y/n]?"
-    read master
-
-    if [ master == "y" ]; then
-        master
-    fi
-}
-
-main
