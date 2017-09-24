3aaa7d641c7a95561373d40e9abcbc5a268c386e
Adam Mertz <adammertz@gmail.com>
2017-09-23 21:25:02 -0500

    added commands to install kubelete and kubeadm

--- install-kub.sh
+++ install-kub.sh
@@ -4,3 +4,10 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s htt
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
 echo "source <(kubectl completion bash)" >> ~/.bashrc
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm
