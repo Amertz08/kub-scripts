c6b728a57cd4948c6517972515812ab40f7682a5
Adam Mertz <adammertz@gmail.com>
2017-09-23 21:41:59 -0500

    added echo statements to provide better feedback

--- install-kub.sh
+++ install-kub.sh
@@ -1,29 +1,31 @@
 #!/usr/bin/env bash
 
 install() {
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-mv ./kubectl /usr/local/bin/kubectl
-echo "source <(kubectl completion bash)" >> ~/.bashrc
-apt-get update && apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-apt-get update
-apt-get install -y kubelet kubeadm
+    echo "Installing Kubernetes"
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+    chmod +x ./kubectl
+    mv ./kubectl /usr/local/bin/kubectl
+    echo "source <(kubectl completion bash)" >> ~/.bashrc
+    apt-get update && apt-get install -y apt-transport-https
+    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+    cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+    deb http://apt.kubernetes.io/ kubernetes-xenial main
+    EOF
+    apt-get update
+    apt-get install -y kubelet kubeadm
 }
 
 
 master() {
-kubectl init --pod-network-cidr
+    echo "Initializing master node"
+    kubeadm init --pod-network-cidr
 }
 
 
 main() {
-    echo "Installing Kubernetes"
     install
 
+    echo ""
     echo "Is this a master node [y/n]?"
     read master
 
